7b9389b236c1369cc5cb39abe76185fe3f4090be
pratik <pratikmitkar12@gamil.com>
2024-02-05 21:32:11 +0530

    new update by pratik

--- app/src/main/java/com/example/androidgpssos/MainActivity.java
+++ app/src/main/java/com/example/androidgpssos/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.androidgpssos;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +14,7 @@ public class MainActivity extends AppCompatActivity {
         addbtn = findViewById(R.id.addcont);
 
 
-        addbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ContactActivity.class));
-            }
-        });
+        addbtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ContactActivity.class)));
 
     }
 }
